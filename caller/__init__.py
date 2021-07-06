23482143fd7ccd4e0fa58dc5c4dc827f7fd1308c
John <john.geraldo.peters@gmail.com>
2021-07-06 11:41:59 -0500

    Added some clean up. 0 macs peaks not in tanoa. Now it's just verifying all of tanoas peaks and then removing all of the non-peaks

--- caller/__init__.py
+++ caller/__init__.py
@@ -1,8 +1,9 @@
 
 from os import pathsep, write
+from posixpath import split
 
 
-def read_and_call(file_name, outputdir, write_depths):
+def read_and_call(file_name, outputdir, write_depths, write_mins):
     """Takes a depth file and calculates where peaks are within that file
 
     Args:
@@ -13,10 +14,19 @@ def read_and_call(file_name, outputdir, write_depths):
     depths = {}
     chromosome = "null"
     output_file = open(outputdir + "/" + "tanoa_called_peaks.peaks", 'w')
-    if write_depths:
-        compressed_depths_output = open(outputdir + "/" + "compressed_depths.depths", 'w')
+    if write_depths or write_mins:
+        if write_depths:
+            compressed_depths_output = open(outputdir + "/" + "compressed_depths.depths", 'w')
+        else:
+            compressed_depths_output = "null"
+        if write_mins:
+            min_output = open(outputdir + "/" + "tanoa_called_mins.minimums", 'w')
+        else:
+            min_output = "null"
     else:
         compressed_depths_output = "null"
+        min_output = "null"
+
     with open(file_name, "r") as file:
         for line in file:
             line.strip()
@@ -32,7 +42,7 @@ def read_and_call(file_name, outputdir, write_depths):
                 print("Compressing " + chromosome)
                 compressed_depths = compress(depths) #Compress the depths to make later algorithms work better
                 depths = {}                           #and faster
-                prossess_and_write_peaks(chromosome, compressed_depths, output_file, write_depths, compressed_depths_output)
+                prossess_and_write_peaks(chromosome, compressed_depths, output_file, write_depths, compressed_depths_output, write_mins, min_output)
                 #####
                 print("Reading in ", ch)
                 chromosome = ch
@@ -40,19 +50,23 @@ def read_and_call(file_name, outputdir, write_depths):
         print("Compressing " + chromosome)
         compressed_depths = compress(depths) #Compress the depths to make later algorithms work better
         depths = {} 
-        prossess_and_write_peaks(chromosome, depths, output_file, write_depths, compressed_depths_output)
+        prossess_and_write_peaks(chromosome, depths, output_file, write_depths, compressed_depths_output, write_mins, min_output)
         #This gets the last chromosome in the file 
 
     output_file.close
     if write_depths:
         compressed_depths_output.close
 
-def prossess_and_write_peaks(chromosome, compressed_depths, output_file, write_depths, compressed_depths_output):
+def prossess_and_write_peaks(chromosome, compressed_depths, output_file, write_depths, compressed_depths_output, write_mins, min_output):
     print("Calling peaks")
     peak_list = call_peaks(compressed_depths) #calls peaks 
+    peak_list = clean_peaks(peak_list)
     write_peaks_to_file(peak_list, output_file, chromosome) #writes them
     if write_depths:
         write_depths_to_file(compressed_depths_output, chromosome, compressed_depths)
+    if write_mins:
+        min_list = call_troughs(compressed_depths)
+        write_peaks_to_file(min_list, min_output, chromosome)
     print("Peaks in " + chromosome + " called and written to file")
 
 def compress(depths):
@@ -86,6 +100,30 @@ def compress(depths):
             jump = False
 
     return depths_compressed
+
+def clean_peaks(peak_list):
+    new_peaks = []
+    for index, peak in enumerate(peak_list):
+        split_peak = peak.split("-")
+        start = split_peak[0]
+        end = split_peak[1]
+        if index != len(peak_list) - 1:
+            next_peak_split = peak_list[index + 1].split("-")
+            start_next = next_peak_split[0]
+            end_next = next_peak_split[1]
+            if int(start_next) - int(end) <= 150:
+                new_peak = start + "-" + end_next
+                new_peaks.append(new_peak)
+            else:
+                if len(new_peaks) != 0:
+                    last_peak = new_peaks[-1]
+                    last_split = last_peak.split("-")
+                    last_end = last_split[1]
+                    if int(end) != int(last_end):
+                        new_peaks.append(peak)
+                else:
+                    new_peaks.append(peak)
+    return new_peaks
 
 def write_peaks_to_file(peaks, output_file, chromosome):
     """Wries a given list of peaks to file
@@ -130,7 +168,7 @@ def call_peaks(compressed_depths):
     for index, key in enumerate(keys):
         current_depth = int(compressed_depths[key])
         if current_depth > prev_depth: #Finds locations where the slope is positive...
-            is_max = verify_max(compressed_depths, keys, 7, keys[index], index)
+            is_max = verify_max(compressed_depths, keys, 3, keys[index], index)
             if is_max: #Checks those for being a maximum. What if I did no checking?
                 peak_list.append(keys[index])
         prev_depth = current_depth
@@ -166,12 +204,49 @@ def check_max(window_keys, compressed_depths, peak_key):
     """
     potential_max = int(compressed_depths[peak_key])
     is_max = True
+
+    if potential_max <= 3:
+        return False
+
+    max_equals = 0
+
     for key in window_keys:
         key_depth = int(compressed_depths[key])
         if key_depth > potential_max:
             is_max = False
+        elif key_depth == potential_max:
+            max_equals += 1
+    if max_equals >= 2:
+        key_depth = False
+
     return is_max
 
+def call_troughs(compressed_depths):
+    keys = list(compressed_depths.keys())
+    trough_list = []
+    prev_depth = 0
+    for index, key in enumerate(keys):
+        current_depth = int(compressed_depths[key])
+        if current_depth < prev_depth: #Finds locations where the slope is negative
+            is_min = verify_min(compressed_depths, keys, 3, keys[index], index)
+            if is_min: #Checks those for being a minimum. This is just the max algo flipped
+                trough_list.append(keys[index])
+        prev_depth = current_depth
+    return trough_list
+
+def verify_min(compressed_depths, keys, steps, peak_key, key_index):
+    window_keys = generate_window(keys, key_index, steps)
+    return check_min(window_keys, compressed_depths, peak_key)
+
+def check_min(window_keys, compressed_depths, peak_key):
+    potential_min = int(compressed_depths[peak_key])
+    is_min = True
+    for key in window_keys:
+        key_depth = int(compressed_depths[key])
+        if key_depth < potential_min:
+            is_min = False
+    return is_min
+    
 def generate_window(keys, key_index, steps):
     """Generates a list (window) steps distance t the left and right of a given peak 
 
@@ -181,7 +256,7 @@ def generate_window(keys, key_index, steps):
         steps (int): How many steps left and write will be searched to verify a local maximum
 
     Returns:
-        [list]: A list of keys that includes all keys steps away from a given peak
+        [list]: A list of keys that includes all keys steps away from a given location
     """
     window_keys = []
     for x in range(1, steps):
